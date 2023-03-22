6b68b0ad8098be4caba9ad42eccb25e5ecc8d19f
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-03-22 17:53:23 -0400

    Create and style page 1 and event listeners for pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,42 @@
 import {basePage} from "./page";
 import {tabOne} from "./page";
+import {tabTwo} from "./page";
+import {tabThree} from "./page";
 import "./style.css"
 
-
 // basePage();
+
+// const page1 = document.querySelector("#page1");
+// const page2 = document.querySelector("#page2");
+// const page3 = document.querySelector("#page3");
+
+// let page1Trigger = true;
+// let page2Trigger = false;
+// let page3Trigger = false;
+
 // tabOne();
+// page1.classList.add('selected');
+
+// page1.addEventListener('click', () => {
+//     if (page1Trigger === false) {
+//         page1.classList.add('selected');
+//         page2.classList.remove('selected');
+//         page3.classList.remove('selected');
+//         tabOne();
+//         page1Trigger = true;
+//         page2Trigger = false;
+//         page3Trigger = false;
+//     }
+// });
+
+// page2.addEventListener('click', () => {
+//     if (page2Trigger === false) {
+//         page2.classList.add('selected');
+//         page1.classList.remove('selected');
+//         page3.classList.remove('selected');
+//         tabTwo();
+//         page2Trigger = true;
+//         page1Trigger = false;
+//         page3Trigger = false;
+//     }
+// });
--- src/page.js
+++ src/page.js
@@ -1,29 +1,43 @@
 function elementFactory(type, class1, class2, id, text) {
     const element = document.createElement(type);
-    
     if (class1) element.classList.add(class1);
     if (class2) element.classList.add(class2);
     if (id) element.setAttribute("id", id);
-    if (text) element.textContent = text;
+    if (text) element.innerHTML = text;
     return element;
 }
 
-const container = document.querySelector("#container");
-
 export function basePage() {
-    container.appendChild(elementFactory('header', '', '', '', ''));
-    container.lastChild.appendChild(elementFactory('div', '', '', 'page1', ''));
-    container.lastChild.lastChild.appendChild(elementFactory('span', '', '', '', 'Home'));
-    container.lastChild.appendChild(elementFactory('div', '', '', 'page2', ''));
-    container.lastChild.lastChild.appendChild(elementFactory('span', '', '', '', 'Menu'));
-    container.lastChild.appendChild(elementFactory('div', '', '', 'page3', ''));
-    container.lastChild.lastChild.appendChild(elementFactory('span', '', '', '', 'About Us'));
-    container.appendChild(elementFactory('div', '', '', 'content', ''));
-    container.appendChild(elementFactory('footer', '', '', '', ''));
+    const container = document.querySelector("#container");
+    container.appendChild(elementFactory('header', undefined, undefined, undefined, undefined));
+    container.lastChild.appendChild(elementFactory('div', undefined, undefined, 'page1', undefined));
+    container.lastChild.lastChild.appendChild(elementFactory('span', undefined, undefined, undefined, 'Home'));
+    container.lastChild.appendChild(elementFactory('div', undefined, undefined, 'page2', undefined));
+    container.lastChild.lastChild.appendChild(elementFactory('span', undefined, undefined, undefined, 'Menu'));
+    container.lastChild.appendChild(elementFactory('div', undefined, undefined, 'page3', undefined));
+    container.lastChild.lastChild.appendChild(elementFactory('span', undefined, undefined, undefined, 'About Us'));
+    container.appendChild(elementFactory('div', undefined, undefined, 'content', undefined));
+    container.appendChild(elementFactory('footer', undefined, undefined, undefined, undefined));
+    container.lastChild.appendChild(elementFactory('div', undefined, undefined, undefined, '<em>*Note I am not the person in the picture. Thanks to Paul Dilett for his likeness</em>'));
 }
 
 export function tabOne() {
     const content = document.querySelector("#content");
-    
+    content.innerHTML = '';
+    content.appendChild(elementFactory('h1', undefined, undefined, undefined, `Welcome to Antwon's Pizzeria!`));
+    const img = elementFactory('img', undefined, undefined, undefined, undefined);
+    img.setAttribute('src', '/src/picture.png')
+    content.appendChild(img);
+    content.appendChild(elementFactory('div', undefined, undefined, 'alt', `<em>Antwon in his kitchen</em>`));
+    content.appendChild(elementFactory('div', undefined, undefined, 'text-container', `<b>When the moon hits your eye and you want a pizza pie</b>, come try the Pizza Tower or any of your favorites 
+    at Antwon's Pizzeria. Antwon D'Maja has been making pizza since he opened his first pizzeria in the year of 1868.`));
+    content.lastChild.appendChild(elementFactory('p', undefined, undefined, undefined, `Though he is not from Italy, he prides his work as an authentic Italian experience and has wowed Pizza
+    Enthusiast critics such as Vinny Vinesauce, Doctor Don Wario M.D, and Jabroni Mike with his Za's. Come try
+    some Pizza today!`));
+}
 
+export function tabTwo() {
+    const content = document.querySelector("#content");
+    content.innerHTML = '';
+    
 }
