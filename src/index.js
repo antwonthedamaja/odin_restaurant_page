6cd9c12745fd93ce9e0b3c738d9dec5e0d8de7a3
AntwonTheDamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-03-22 22:48:27 -0400

    Complete coding of second page

--- src/index.js
+++ src/index.js
@@ -1,42 +1,35 @@
 import {basePage} from "./page";
 import {tabOne} from "./page";
 import {tabTwo} from "./page";
-import {tabThree} from "./page";
 import "./style.css"
 
-// basePage();
+basePage();
+const page1 = document.querySelector("#page1");
+const page2 = document.querySelector("#page2");
 
-// const page1 = document.querySelector("#page1");
-// const page2 = document.querySelector("#page2");
-// const page3 = document.querySelector("#page3");
-
-// let page1Trigger = true;
-// let page2Trigger = false;
-// let page3Trigger = false;
+let page1Trigger = true;
+let page2Trigger = false;
 
 // tabOne();
 // page1.classList.add('selected');
+tabOne();
 
-// page1.addEventListener('click', () => {
-//     if (page1Trigger === false) {
-//         page1.classList.add('selected');
-//         page2.classList.remove('selected');
-//         page3.classList.remove('selected');
-//         tabOne();
-//         page1Trigger = true;
-//         page2Trigger = false;
-//         page3Trigger = false;
-//     }
-// });
+page1.addEventListener('click', () => {
+    if (page1Trigger === false) {
+        page1.classList.add('selected');
+        page2.classList.remove('selected');
+        tabOne();
+        page1Trigger = true;
+        page2Trigger = false;
+    }
+});
 
-// page2.addEventListener('click', () => {
-//     if (page2Trigger === false) {
-//         page2.classList.add('selected');
-//         page1.classList.remove('selected');
-//         page3.classList.remove('selected');
-//         tabTwo();
-//         page2Trigger = true;
-//         page1Trigger = false;
-//         page3Trigger = false;
-//     }
-// });
+page2.addEventListener('click', () => {
+    if (page2Trigger === false) {
+        page2.classList.add('selected');
+        page1.classList.remove('selected');
+        tabTwo();
+        page2Trigger = true;
+        page1Trigger = false;
+    }
+});
--- src/page.js
+++ src/page.js
@@ -14,8 +14,6 @@ export function basePage() {
     container.lastChild.lastChild.appendChild(elementFactory('span', undefined, undefined, undefined, 'Home'));
     container.lastChild.appendChild(elementFactory('div', undefined, undefined, 'page2', undefined));
     container.lastChild.lastChild.appendChild(elementFactory('span', undefined, undefined, undefined, 'Menu'));
-    container.lastChild.appendChild(elementFactory('div', undefined, undefined, 'page3', undefined));
-    container.lastChild.lastChild.appendChild(elementFactory('span', undefined, undefined, undefined, 'About Us'));
     container.appendChild(elementFactory('div', undefined, undefined, 'content', undefined));
     container.appendChild(elementFactory('footer', undefined, undefined, undefined, undefined));
     container.lastChild.appendChild(elementFactory('div', undefined, undefined, undefined, '<em>*Note I am not the person in the picture. Thanks to Paul Dilett for his likeness</em>'));
@@ -32,12 +30,30 @@ export function tabOne() {
     content.appendChild(elementFactory('div', undefined, undefined, 'text-container', `<b>When the moon hits your eye and you want a pizza pie</b>, come try the Pizza Tower or any of your favorites 
     at Antwon's Pizzeria. Antwon D'Maja has been making pizza since he opened his first pizzeria in the year of 1868.`));
     content.lastChild.appendChild(elementFactory('p', undefined, undefined, undefined, `Though he is not from Italy, he prides his work as an authentic Italian experience and has wowed Pizza
-    Enthusiast critics such as Vinny Vinesauce, Doctor Don Wario M.D, and Jabroni Mike with his Za's. Come try
+    Enthusiast critics such as Vinny Vinesauce, Doctor Don Wario M.D, and Jabroni Mike with his famous Za's. Come try
     some Pizza today!`));
 }
 
 export function tabTwo() {
     const content = document.querySelector("#content");
     content.innerHTML = '';
-    
+    content.appendChild(elementFactory('h1', undefined, undefined, 'menu', `Menu`));
+    content.appendChild(elementFactory('div', undefined, undefined, 'item1', undefined));
+    const img1 = elementFactory('img', undefined, undefined, undefined, undefined);
+    img1.setAttribute('src', '/src/item1.jpg');
+    img1.setAttribute('class', 'img');
+    content.lastChild.appendChild(img1);
+    content.lastChild.appendChild(elementFactory('div', undefined, undefined, undefined, 'The Pizza Tower: <b><u>$79.99</b></u>'));
+    content.appendChild(elementFactory('div', undefined, undefined, 'item2', undefined));
+    const img2 = elementFactory('img', undefined, undefined, undefined, undefined);
+    img2.setAttribute('src', '/src/item2.png');
+    img2.setAttribute('class', 'img');
+    content.lastChild.appendChild(img2);
+    content.lastChild.appendChild(elementFactory('div', undefined, undefined, undefined, 'Pizza Ball: <b><u>$19.99</b></u>'));
+    content.appendChild(elementFactory('div', undefined, undefined, 'item3', undefined));
+    const img3 = elementFactory('img', undefined, undefined, undefined, undefined);
+    img3.setAttribute('src', '/src/item3.png');
+    img3.setAttribute('class', 'img');
+    content.lastChild.appendChild(img3);
+    content.lastChild.appendChild(elementFactory('div', undefined, undefined, undefined, `'Pizza the Hutt' Pizza Mountain: <b><u>$124.99</b></u>`));
 }
